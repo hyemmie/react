4f7e04ba3d506077faebbc7526fbcac581044dc1
Sangwon <born9507@snu.ac.kr>
2020-10-01 19:05:20 +0900

    Update from 11 to 18

--- sangwon/begin-react/src/App.js
+++ sangwon/begin-react/src/App.js
@@ -1,36 +1,142 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
+// import Hello from './Hello';
+// import Wrapper from './Wrapper';
 // import New from './New';
-import Counter from './Counter';
-import InputSample from './InputSample';
+// import Counter from './Counter';
+// import InputSample from './InputSample';
+import UserList from './UserList'
+import CreateUser from './CreateUser'
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
-  const name = 'react'
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  
+  const { username, email } = inputs;
+  
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = useCallback(() => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+    
+    setInputs({
+      username: '',
+      email: ''
+    });
+
+    nextId.current += 1;
+  }, [users, username, email])
+  // 함수에서 사용하는 state 나 props 를 반드시 deps 안에 포함시켜야 한다
+  
+
+  const onRemove = useCallback(
+    id => {
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  )
+
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id? {...user, active: !user.active} : user
+        )
+      )
+    },
+    [users]
+  )
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  // deps 배열 안의 내용이 바뀔 때 앞의 함수를 호출한다.
+
   return (
     <>
-      {/* 주석은 화면에 보이지 않습니다 */}
-      <Wrapper>
-        <Hello name="react" color="red" isSpecial/>
-        <Hello color="pink"/>  
-        {/* 이렇게 안으로 들어간 것들도 다 props 인데,
-        이를 children 이라고 한다. */}
-      </Wrapper>
-
-      <Counter />
-
-      <InputSample />
-      
-      {/* <div style={style}>name: {name}</div>
-      <div className="gray-box"></div> */}
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수: {count}</div>
     </>
-  );
+  )
+
 }
 
 export default App;
+
+
+// function App() {
+//   const name = 'react'
+//   const style = {
+//     backgroundColor: 'black',
+//     color: 'aqua',
+//     fontSize: 24,
+//     padding: '1rem'
+//   }
+//   return (
+//     <>
+//       {/* 주석은 화면에 보이지 않습니다 */}
+//       <Wrapper>
+//         <Hello name="react" color="red" isSpecial/>
+//         <Hello color="pink"/>  
+//         {/* 이렇게 안으로 들어간 것들도 다 props 인데,
+//         이를 children 이라고 한다. */}
+//       </Wrapper>
+
+//       <Counter />
+
+//       <InputSample />
+      
+//       {/* <div style={style}>name: {name}</div>
+//       <div className="gray-box"></div> */}
+//     </>
+//   );
+// }
+
+// export default App;
--- sangwon/begin-react/src/UserList.js
+++ sangwon/begin-react/src/UserList.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function User({user}) {
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('user 값이 설정됨');
+    console.log(user);
+    return () => {
+      console.log("user 가 바뀌기 전..");
+      console.log(user);
+    };
+  }, []);
+  // active 가 바뀔 때는 두번 렌더가 일어난다
+  // deps 를 생략하면 변화가 일어날때마다 리렌더
+
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b> 
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   )
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'publice@.com'
-    },
-    {
-      id: 2,
-      username: 'velopert',
-      email: 'publice@.com'        
-    }
-    {
-      id: 3,
-      username: 'velopert',
-      email: 'publice@.com'        
-    }
-  ];
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
-      {users.map((user, index) => (
-        <User user={user} key={index} />
+      {users.map(user => (
+        <User 
+          user={user} 
+          key={user.id} 
+          onRemove={onRemove} 
+          onToggle={onToggle}
+        />
       ))}
     </div>
   )
