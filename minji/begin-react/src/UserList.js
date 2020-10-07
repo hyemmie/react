0aeaad1e720753027672c8008ffda102cb3ee4c1
Minji Kim <minji1004y@gmail.com>
2020-10-07 16:27:46 +0900

    Goooo

--- minji/begin-react/src/UserList.js
+++ minji/begin-react/src/UserList.js
@@ -1,61 +1,47 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
-    return (
-        <div>
-            <b 
-            style={{
-                cursor:'pointer',
-                color: user.active ? 'green':'black'}}
-            onClick={()=> onToggle(user.id)}
-            >
-            {user.username}</b> 
-            
-            <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
-        </div>
-    )
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log("user 값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user 가 바뀌기 전..");
+      console.log(user);
+    };
+  }, [user]);
+  console.log("renderUser");
+
+  return (
+    <div>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+});
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
 
-function UserList({users, onRemove, onToggle}) {
-    // const users = [
-    //     {
-    //         id:1,
-    //         username: 'minji',
-    //         email:'minji@test.com',
-        
-    //     },
-    //     {
-    //         id:2,
-    //         username: 'kihyun',
-    //         email:'kihyun@test.com',
-        
-    //     },
-    //     {
-    //         id:3,
-    //         username: 'haeun',
-    //         email:'haeun@test.com',
-        
-    //     }
-    // ];
-
-    return (
-        <div>
-            {/* <div>
-                <b>{users[0].username}</b> <span> ({users[0].email}) </span>
-            </div>
-            <div>
-                <b>{users[1].username}</b> <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b> <span>({users[2].email})</span>
-            </div> */}
-            {users.map((user) => (
-                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
-            ))}
-        </div>
-        // 콜백함수의 두번쨰 파라미터는 무조건 key값으로 적용가능한가??
-    )
-}
-
-export default UserList;
+export default React.memo(UserList);
